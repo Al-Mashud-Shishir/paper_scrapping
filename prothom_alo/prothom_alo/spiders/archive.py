ab51d3849366b3bc4afc12418818215747a4d4ce
Al Mashud Shishir <almashudmaddi@gmail.com>
2019-07-24 22:17:51 +0600

    date and page wise archive link finding done

--- prothom_alo/prothom_alo/spiders/archive.py
+++ prothom_alo/prothom_alo/spiders/archive.py
@@ -9,6 +9,26 @@ class ArchiveSpider(scrapy.Spider):
     start_urls = ['https://prothomalo.com/archive/']
 
     def parse(self, response):
+        years = [str(i) for i in range(2013, 2020)]
+        months = [str(i) for i in range(1, 13)]
+        dates = [str(i) for i in range(1, 32)]
+
+        for y in years:
+            for m in months:
+                for d in dates:
+                    dateUrl = y+"-"+m+"-"+d
+                    datewise_archive_url = urljoin(response.url, dateUrl)
+
+                    yield scrapy.Request(datewise_archive_url, callback=self.parse_archive)
+
+                    for p in range(2, 100):
+                        page_url = "?page="+str(p)
+                        datewise_archive_page_url = urljoin(
+                            datewise_archive_url, page_url)
+                        yield scrapy.Request(datewise_archive_url, callback=self.parse_archive)
+
+    def parse_archive(self, response):
+
         article_div = response.xpath('//div[@class="listing"]')
         if article_div:
             article_links = article_div.xpath(
